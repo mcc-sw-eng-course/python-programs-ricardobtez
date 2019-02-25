db549cc64b3d16cf16165716a45bfb05b0655c44
Ricardo Benitez <benitezc.ricardo@gmail.com>
2019-02-24 18:48:52 -0600

    set_input_data finished. Coverage 95%
    Signed-off-by: Ricardo Benitez <benitezc.ricardo@gmail.com>

--- L6Exercises/test_sortCSV.py
+++ L6Exercises/test_sortCSV.py
@@ -2,12 +2,13 @@
 # Student-Id: A01018084
 
 import unittest
-from os import path, makedirs, remove, rmdir
+from os import path, makedirs, remove, rmdir, chmod
 from sortCSV import *
 
 VALID_INPUT_FILENAME = 'inputFile.csv'
 INVALID_INPUT_FILENAME = 'invalidInputFile.csv'
 INVALID_INPUT_NAME_FILENAME = 'invalidInFilaname.csv.tmp'
+INVALID_PERMISSIONS_FILE = 'invalidPermissions.csv'
 
 VALID_OUTPUT_FILENAME = 'outputFile.csv'
 INVALID_OUTPUT_FILENAME = 'invalidOutputFile.csv'
@@ -16,50 +17,56 @@ INVALID_OUTPUT_NAME_FILENAME = 'invalidOutFilaname.csv.tmp'
 NEW_FOLDER_PATH = 'tmpFolder'
 
 class test_sortCSV(unittest.TestCase):
+
+    def setUp(self):
+        self.tmpClass = sortCSV()
+
     def test_classCreation(self):
         tmpClass = sortCSV()
         self.assertIsNotNone(tmpClass)
 
     # Positive testing set input data function
     def test_validInputFile_SetInputData(self):
-        tmpClass = sortCSV()
         inputFileHdlr = open(VALID_INPUT_FILENAME,'w')
         inputFileHdlr.write('1,2,3,4')
         inputFileHdlr.close()
-        self.assertEqual(sdError.E_OK, tmpClass.set_input_data(VALID_INPUT_FILENAME))
+        self.assertEqual(sdError.E_OK, self.tmpClass.set_input_data(VALID_INPUT_FILENAME))
         remove(VALID_INPUT_FILENAME)
     def test_validInputFile_SetInputData_OtherPath(self):
-        tmpClass = sortCSV()
         if (not path.exists(NEW_FOLDER_PATH)):
             makedirs(NEW_FOLDER_PATH)
         inputFileHdlr = open(NEW_FOLDER_PATH + '/' + VALID_INPUT_FILENAME,'w')
         inputFileHdlr.write('1,2,3,4')
         inputFileHdlr.close()
-        self.assertEqual(sdError.E_OK, tmpClass.set_input_data(NEW_FOLDER_PATH + '/' + VALID_INPUT_FILENAME))
+        self.assertEqual(sdError.E_OK, self.tmpClass.set_input_data(NEW_FOLDER_PATH + '/' + VALID_INPUT_FILENAME))
         remove(NEW_FOLDER_PATH +'/'+VALID_INPUT_FILENAME)
         rmdir(NEW_FOLDER_PATH)
 
     # Negative testing input file
     # def test_invalidInputFile_SetInputData(self):
-    #     tmpClass = sortCSV()
     #     inputFileHdlr = open(INVALID_INPUT_FILENAME,'w')
     #     inputFileHdlr.write('1,2,3,a,b,c')
     #     inputFileHdlr.close()
     #     self.assertEqual(sdError.E_INVALID_DATA_IN_FILE, tmpClass.set_input_data(INVALID_INPUT_FILENAME))
     #     remove(INVALID_INPUT_FILENAME)
     def test_invalidFileName_SetInputData(self):
-        tmpClass = sortCSV()
         inputFileHdlr = open(INVALID_INPUT_NAME_FILENAME, 'w')
         inputFileHdlr.write('1,2,3,4')
         inputFileHdlr.close()
-        self.assertEqual(sdError.E_NOT_VALID_FILE, tmpClass.set_input_data(INVALID_INPUT_NAME_FILENAME))
+        self.assertEqual(sdError.E_NOT_VALID_FILE, self.tmpClass.set_input_data(INVALID_INPUT_NAME_FILENAME))
         remove(INVALID_INPUT_NAME_FILENAME)
     def test_fileNotExistent_SetInputData(self):
-        tmpClass = sortCSV()
-        self.assertEqual(sdError.E_NOT_VALID_FILE, tmpClass.set_input_data('some'+VALID_INPUT_FILENAME))
+        self.assertEqual(sdError.E_NOT_VALID_FILE, self.tmpClass.set_input_data('tmp'+VALID_INPUT_FILENAME))
     def test_tuppleInsteadOfStr_SetInputData(self):
-        tmpClass = sortCSV()
-        self.assertRaises(ValueError, tmpClass.set_input_data, [VALID_INPUT_FILENAME])
+        self.assertRaises(ValueError, self.tmpClass.set_input_data, [VALID_INPUT_FILENAME])
+    def test_noReadPermission_SetInputData(self):
+        inputFileHdlr = open(INVALID_PERMISSIONS_FILE,'w')
+        inputFileHdlr.write('1,2,3,4')
+        inputFileHdlr.close()
+        chmod(INVALID_PERMISSIONS_FILE, 0o000)
+        self.assertEqual(sdError.E_NOT_READ_PERMISSION, self.tmpClass.set_input_data(INVALID_PERMISSIONS_FILE))
+        chmod(INVALID_PERMISSIONS_FILE, 0o777)
+        remove(INVALID_PERMISSIONS_FILE)
 
 
 
