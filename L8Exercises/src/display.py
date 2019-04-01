adc505099ab3e5f58043aeb54aa376fb4dd0c787
Ricardo Benitez <benitezc.ricardo@gmail.com>
2019-04-01 16:49:38 -0600

    Adding display class as a Singleton
    Signed-off-by: Ricardo Benitez <benitezc.ricardo@gmail.com>

--- L8Exercises/src/display.py
+++ L8Exercises/src/display.py
@@ -5,13 +5,29 @@
 import socket
 
 class Display:
+
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method"""
+        if (Display.__instance == None):
+            Display()
+        return Display.__instance
+
     def __init__(self):
+        """ Virtually private constructor"""
+        if (Display.__instance != None):
+            raise Exception("This class is a singleton!")
+        else:
+            Display.__instance = self
+
         self.comSocket = None
 
-        if (True == boTwoPlayers):
-            self.turn = enUser.PLAYER_ONE
-            self.comSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.comSocket.bind((socket.gethostname(), 8787))
+        # if (True == boTwoPlayers):
+        #     self.turn = enUser.PLAYER_ONE
+        #     self.comSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #     self.comSocket.bind((socket.gethostname(), 8787))
             # self.comSocket.listen(5)
 
     def display(self, board: Board, boLocal: bool=True):
@@ -51,3 +67,9 @@ class Display:
             print(data)
         else:
             print(data)
+
+if __name__ == '__main__':
+    d = Display.getInstance()
+    print(d)
+    e = Display.getInstance()
+    print(d)
